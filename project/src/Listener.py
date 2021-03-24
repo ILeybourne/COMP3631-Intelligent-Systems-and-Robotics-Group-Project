6990f7fdfaa8113d6a77a660b370d4045e628a11
ILeybourne <izzywu@hotmail.com>
2021-03-24 22:53:51 +0000

    update listener to recieve character bools

--- project/src/Listener.py
+++ project/src/Listener.py
@@ -39,17 +39,38 @@
 import rospy
 from std_msgs.msg import Bool
 
+
 def callbackRed(data):
     # rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
     print("Red flag is " + str(data.data))
+
 
 def callbackGreen(data):
     # rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
     print("Green flag is " + str(data.data))
 
 
-def listener():
+def callbackMustard(data):
+    # rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+    print("callbackMustard " + str(data.data))
 
+
+def callbackPeacock(data):
+    # rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+    print("callbackPeacock " + str(data.data))
+
+
+def callbackPlum(data):
+    # rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+    print("callbackPlum " + str(data.data))
+
+
+def callbackScarlet(data):
+    # rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+    print("callbackScarlet " + str(data.data))
+
+
+def listener():
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
@@ -58,9 +79,14 @@ def listener():
     rospy.init_node('listener', anonymous=True)
     sub = rospy.Subscriber('red_circle_topic', Bool, callbackRed)
     sub = rospy.Subscriber('green_circle_topic', Bool, callbackGreen)
+    sub = rospy.Subscriber('mustard_topic', Bool, callbackMustard)
+    sub = rospy.Subscriber('peacock_topic', Bool, callbackPeacock)
+    sub = rospy.Subscriber('plum_topic', Bool, callbackPlum)
+    sub = rospy.Subscriber('scarlet_topic', Bool, callbackScarlet)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
+
 
 if __name__ == '__main__':
     listener()
