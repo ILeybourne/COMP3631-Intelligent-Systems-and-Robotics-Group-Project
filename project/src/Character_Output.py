1a67e672c5e291432c49cd9f94e1beb0f8a58a6a
ILeybourne <izzywu@hotmail.com>
2021-03-30 17:13:17 +0100

    capturing character image and writing to text file in separate file

--- project/src/Character_Output.py
+++ project/src/Character_Output.py
@@ -4,14 +4,13 @@
 
 from __future__ import division
 import cv2
-import cv
 import numpy as np
 import rospy
 import sys
 
 from geometry_msgs.msg import Twist, Vector3, Pose, Point, Quaternion
 from sensor_msgs.msg import Image
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, Int32MultiArray
 from cv_bridge import CvBridge, CvBridgeError
 from actionlib_msgs.msg import *
 
@@ -19,14 +18,23 @@ from actionlib_msgs.msg import *
 class CharacterOutput():
     def __init__(self):
         self.bridge = CvBridge()
+        # Subscribers
+        self.sub_mustard_topic = rospy.Subscriber('mustard_topic', Bool, self.callbackMustard)
+        self.sub_peacock_topic = rospy.Subscriber('peacock_topic', Bool, self.callbackPeacock)
+        self.sub_plum_topic = rospy.Subscriber('plum_topic', Bool, self.callbackPlum)
+        self.sub_scarlet_topic = rospy.Subscriber('scarlet_topic', Bool, self.callbackScarlet)
+        self.sub_rectangle_topic = rospy.Subscriber('rectangle_topic', Bool, self.callbackRectangle)
+        self.sub_rectangle_in_bounds_topic = rospy.Subscriber('rectangle_in_bounds_topic', Bool,
+                                                              self.callbackRectangleInBounds)
+        self.sub_rectangle_ints_topic = rospy.Subscriber('rectangle_ints_topic', Int32MultiArray,
+                                                         self.callbackRectangleInts)
         self.sub = rospy.Subscriber('image_topic', Image, self.imageCallback)
-        sub = rospy.Subscriber('mustard_topic', Bool, callbackMustard)
-        sub = rospy.Subscriber('peacock_topic', Bool, callbackPeacock)
-        sub = rospy.Subscriber('plum_topic', Bool, callbackPlum)
-        sub = rospy.Subscriber('scarlet_topic', Bool, callbackScarlet)
-        sub = rospy.Subscriber('rectangle_topic', Bool, callbackRectangle)
+
+        self.pub_shutdown_all_nodes = rospy.Publisher('shutdown_topic', Bool, queue_size=10)
 
         self.character_printed = False
+        self.character_captured = False
+        self.shutdown_all_nodes = False
 
         # Initialise sensitivity variable for colour detection
         self.r_sensitivity = 5
@@ -61,25 +69,74 @@ class CharacterOutput():
         self.image_x = self.cv_image.shape[1]
         self.image_y = self.cv_image.shape[0]
 
-    def callbackMustard(data):
-        print("callbackMustard " + str(data.data))
+    def callbackMustard(self, data):
+        self.mustard = data.data
 
-    def callbackPeacock(data):
-        print("callbackPeacock " + str(data.data))
+    def callbackPeacock(self, data):
+        self.peacock = data.data
 
-    def callbackPlum(data):
-        print("callbackPlum " + str(data.data))
+    def callbackPlum(self, data):
+        self.plum = data.data
 
-    def callbackScarlet(data):
-        print("callbackScarlet " + str(data.data))
+    def callbackScarlet(self, data):
+        self.scarlet = data.data
 
-    def callbackRectangle(data):
-        print("callbackRectangle " + str(data.data))
+    def callbackRectangle(self, data):
+        self.rectangle = data.data
 
+    def callbackRectangleInBounds(self, data):
+        self.rectangle_in_bounds = data.data
+
+    def callbackRectangleInts(self, data):
+        self.rectangle_ints = data.data
+
+    def captureImage(self):
+        if self.rectangle and self.rectangle_in_bounds and (self.mustard or self.peacock or self.plum or self.scarlet):
+            cv2.imwrite('cluedo_character.png', self.cv_image)
+            self.character_captured = True
+
+    def writeCharacter(self):
+        if self.mustard:
+            if not self.character_printed:
+                self.character_printed = True
+                file = open("cluedo_character.txt", "w")
+                file.write("Colonel Mustard")
+                file.close()
+                self.character_printed = True
+
+        if self.peacock:
+            if not self.character_printed:
+                self.character_printed = True
+                file = open("cluedo_character.txt", "w")
+                file.write("Mrs Peacock")
+                file.close()
+                self.character_printed = True
+
+        if self.plum:
+            if not self.character_printed:
+                self.character_printed = True
+                file = open("cluedo_character.txt", "w")
+                file.write("Professor Plum")
+                file.close()
+                self.character_printed = True
+
+        if self.scarlet:
+            if not self.character_printed:
+                self.character_printed = True
+                file = open("cluedo_character.txt", "w")
+                file.write("Miss Scarlett")
+                file.close()
+                self.character_printed = True
+
+    def shutdownAllNodes(self):
+        if self.character_printed and self.character_captured:
+            self.pub_shutdown_all_nodes.publish(True)
+        else:
+            self.pub_shutdown_all_nodes.publish(False)
 
 def main(args):
     rospy.init_node('character_output', anonymous=True)
-    cI = CharacterOutput()
+    cO = CharacterOutput()
     print("Initializing character finder")
     try:
         rospy.spin()
