2a503bc9f3bb5e7ffa400441074c7fa8f5805cbd
Thomas Marshall <ed17t2m@leeds.ac.uk>
2021-04-01 09:15:40 +0100

    add subscriber to rectangle_topic and corresponding flag

--- project/src/navigating_green_room.py
+++ project/src/navigating_green_room.py
@@ -8,14 +8,33 @@ from std_msgs.msg import Bool
 
 class greenNavigation():
     def __init__(self):
-        # needs a publisher to determine whether the find is in green room 
+        #internal flag for handling execution
         self.in_green_room = False
+        self.nav_completed = False
+        self.rectangle_flag = False
+        #publisher to determine when to start execution
         self.green_room_flag_sub = rospy.Subscriber('inside_green_room', Bool, self.greenRoomFlagCallback)
+        self.rectangle_detection_sub = rospy.Subscriber('rectangle_topic', Bool, self.rectangleFlagCallback)
 
-    def greenRoomFlagCallback(data):
+    # inside_green_room subscriber callback
+    def greenRoomFlagCallback(self, data):
         self.in_green_room = data.data
 
-    
+    # rectangle_tobpic subscriber callback
+    def rectangleFlagCallback(self, data):
+        self.rectangle_flag = data.data
+
+    # main navigation function
+    def startNavigation(self):
+        while(not rospy.is_shutdown()):
+            if (self.in_green_room == True):
+                print("begin action")
+                
+
+            else:
+                #debugging print for main function idling
+                print("waiting...")
+
 
 def main (args):
     rospy.init_node('green_room_navigator', anonymous=True)
