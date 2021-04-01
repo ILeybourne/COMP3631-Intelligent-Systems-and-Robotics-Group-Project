34d92efac974ae90250aa17bd1086c11baa67914
Thomas Marshall <ed17t2m@leeds.ac.uk>
2021-04-01 09:48:24 +0100

    add function for searching scene from current pose for portrait

--- project/src/navigating_green_room.py
+++ project/src/navigating_green_room.py
@@ -5,6 +5,7 @@ import sys
 from geometry_msgs.msg import Twist, Vector3, Pose, Point, Quaternion
 from std_msgs.msg import Bool
 
+PI = 3.1415926535897
 
 class greenNavigation():
     def __init__(self):
@@ -12,9 +13,17 @@ class greenNavigation():
         self.in_green_room = False
         self.nav_completed = False
         self.rectangle_flag = False
-        #publisher to determine when to start execution
+
+        #subscribers to determine when to start execution
         self.green_room_flag_sub = rospy.Subscriber('inside_green_room', Bool, self.greenRoomFlagCallback)
         self.rectangle_detection_sub = rospy.Subscriber('rectangle_topic', Bool, self.rectangleFlagCallback)
+
+        #Publishers
+        self.movement_pub = rospy.Publisher('mobile_base/commands/velocity', Twist, queue_size=10)
+
+        #simple movement to rotate around scene
+        self.rotate = Twist()
+        self.rotate.angular.z = 10*(PI/180)
 
     # inside_green_room subscriber callback
     def greenRoomFlagCallback(self, data):
@@ -28,19 +37,37 @@ class greenNavigation():
     def startNavigation(self):
         while(not rospy.is_shutdown()):
             if (self.in_green_room == True):
-                print("begin action")
+                rotation = 0
                 
-
+                
             else:
                 #debugging print for main function idling
                 print("waiting...")
+    
+
+
+
+    
+    # rotates around current pose to look for character in the scene
+    # returns True if found, False otherwise
+    def lookForRectange(self):
+        rotation = 0
+        while (rotation < 360 and self.rectangle_flag == False):
+                    self.movement_pub.publish(self.rotate)
+                    #sleep after issuing rotation, give chance for message and subsequent processing by other nodes
+                    rospy.sleep(1)
+        
+        if self.rectangle_flag == False:
+            return False
+        else:
+            return True
 
 
 def main (args):
     rospy.init_node('green_room_navigator', anonymous=True)
     gNav = greenNavigation()
     try:
-        rospy.spin()
+        gNav.greenNavigation()
     except rospy.ROSInterruptException:
         pass
 
